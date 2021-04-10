93af8a8389ebc7232ca33688eda474659746933a
mohammedriswan <81406486+mohammedriswan@users.noreply.github.com>
2021-04-10 16:12:22 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,19 @@
 class script(object):
-    START_TEXT = """ **I'm a Simple File Renamer Bot With Permanent Thumbnail Support and I Can also Rename Unlimited Files at the Same Time** 😇 """
+    START_TEXT = """ I'm a Simple File Renamer Bot With Permanent Thumbnail Support and I Can also Rename Unlimited Files at the Same Time 😇 """
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "🌀 **Please Contact @Henry_RichardSon**"
+    UPGRADE_TEXT = "🌀 Please Contact @Henry_RichardSon"
     DOWNLOAD_START = "**Give Me Some Time...**"
     UPLOAD_START = "**Starting to upload...**"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @Henry_RichardSon **"
     SAVED_THUMB = "**Thumbnail Saved ✔ This Is Permanent**"
     DEL_THUMB = "**Thumbnail cleared succesfully!** ✔"
     NO_THUMB = "**No thumbnails found...**"
-    SAVED_RECVD_DOC_FILE = "**File Downloaded Successfully **☺"
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully ☺"
     CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """** 🌀 Follow These steps 🌀 **
+    HELP_USER = """ 🌀 Follow These steps 🌀 
+    
+  
 
 1) Send me the File to Rename
 2) Click 📝 RENAME 📝 Button
