d1b5561b5a4eb1396ed8e0b32a4d05c8c90c8056
mohammedriswan <81406486+mohammedriswan@users.noreply.github.com>
2021-04-11 10:05:09 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 class script(object):
-    START_TEXT = """** I'm a Simple File Renamer Bot With Permanent Thumbnail Support and I Can also Rename Unlimited Files at the Same Time 😇 **"""
+    START_TEXT = """ I'm a Simple File Renamer Bot With Permanent Thumbnail Support and I Can also Rename Unlimited Files at the Same Time 😇 """
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
     UPGRADE_TEXT = """🌀 My Name:- Rename Bot
